ca4b4a906098520f000389edc5ce78cd901fc13c
jakob-i-am <0xjakob@gmail.com>
2025-01-05 16:44:17 +1100

    added avatars for users editing document

---
+++ components/Avatars.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { useOthers, useSelf } from "@liveblocks/react/suspense";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+export default function Avatars() {
+    const others = useOthers();
+    const self = useSelf();
+
+    const all = [self, ...others];
+
+    return (
+        <div className='flex gap-2 items-center'>
+            <p className='font-light text-sm'>Users currently editing this page</p>
+
+            <div className='flex -space-x-5'>
+                {all.map((other, index) => (
+                    <TooltipProvider key={other?.id + index}>
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <Avatar className='border-2 hover:z-10'>
+                                    <AvatarImage src={other?.info.avatar} />
+                                    <AvatarFallback>{other?.info.name}</AvatarFallback>
+                                </Avatar>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p>{self?.id === other?.id ? 'You' : other?.info.name}</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                ))}
+            </div>
+        </div>
+    );
+}
--- components/Document.tsx
+++ components/Document.tsx
@@ -10,66 +10,67 @@ import useOwner from '@/lib/useOwner';
 import DeleteDocument from '@/components/DeleteDocument';
 import InviteUser from '@/components/InviteUser';
 import ManageUsers from '@/components/ManageUsers';
+import Avatars from '@/components/Avatars';
 
 export default function Document({ id }: { id: string }) {
-  const [data, loading, error] = useDocumentData(doc(db, 'documents', id));
-  const [input, setInput] = useState<string>('');
-  const [isUpdating, startTransition] = useTransition();
-  const isOwner = useOwner();
+    const [data, loading, error] = useDocumentData(doc(db, 'documents', id));
+    const [input, setInput] = useState<string>('');
+    const [isUpdating, startTransition] = useTransition();
+    const isOwner = useOwner();
 
-  useEffect(() => {
-    if (!loading && data) {
-      setInput(data.title);
-    }
-  }, [data]);
+    useEffect(() => {
+        if (!loading && data) {
+            setInput(data.title);
+        }
+    }, [data]);
 
-  const updateTitle = (e: FormEvent) => {
-    e.preventDefault();
+    const updateTitle = (e: FormEvent) => {
+        e.preventDefault();
 
-    if (input.trim()) {
-      startTransition(async () => {
-        await updateDoc(doc(db, 'documents', id), {
-          title: input,
-        });
-      });
-    }
-  };
+        if (input.trim()) {
+            startTransition(async () => {
+                await updateDoc(doc(db, 'documents', id), {
+                    title: input,
+                });
+            });
+        }
+    };
 
-  return (
-    <div className='flex-1 h-full bg-white p-5'>
-      <div className='flex max-w-6xl mx-auto justify-between pb-5'>
-        <form
-          onSubmit={updateTitle}
-          className='flex flex-1 space-x-2'
-        >
-          <Input
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-          />
-          <Button
-            disabled={isUpdating}
-            type='submit'
-          >
-            {isUpdating ? 'Updating...' : 'Update'}
-          </Button>
+    return (
+        <div className='flex-1 h-full bg-white p-5'>
+            <div className='flex max-w-6xl mx-auto justify-between pb-5'>
+                <form
+                    onSubmit={updateTitle}
+                    className='flex flex-1 space-x-2'
+                >
+                    <Input
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                    />
+                    <Button
+                        disabled={isUpdating}
+                        type='submit'
+                    >
+                        {isUpdating ? 'Updating...' : 'Update'}
+                    </Button>
 
-          {isOwner && (
-            <>
-              <InviteUser />
-              <DeleteDocument />
-            </>
-          )}
-        </form>
-      </div>
+                    {isOwner && (
+                        <>
+                            <InviteUser />
+                            <DeleteDocument />
+                        </>
+                    )}
+                </form>
+            </div>
 
-      <div className='flex max-w-6xl mx-auto justify-between items-center mb-5'>
-        <ManageUsers />
-        {/* Avatars */}
-      </div>
+            <div className='flex max-w-6xl mx-auto justify-between items-center mb-5'>
+                <ManageUsers />
+                <Avatars />
+            </div>
 
-      <hr className='pb-10' />
+            <hr className='pb-10' />
 
-      <Editor />
-    </div>
-  );
+            <Editor />
+        </div>
+    );
 }
